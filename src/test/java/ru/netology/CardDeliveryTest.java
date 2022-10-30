523bcb53c3da3743e374fe717875705f7009575e
Антаков Сергей <antakov87@mail.ru>
2022-10-30 10:36:10 +0400

    edit elimination of comments

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -14,21 +14,35 @@ import static com.codeborne.selenide.Selenide.*;
 
 
 public class CardDeliveryTest {
-    LocalDate localDate = LocalDate.now().plusDays(3);
-    DateTimeFormatter data = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    String strData = localDate.format(data);
+
+//    Код для генерации даты можно вынести в отдельный метод, передавать в него количество дней,
+//    а возвращать отформатированную строку с датой. Метод может выглядеть примерно так:
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+//    указанный метод принимает количество дней сдвига относительно текущей даты,
+//    а возвращает отформатированную строку с датой. Вы получите возможность генерировать различные даты, как валидные, так и невалидные.
+//    Сохранить полученную строку можно в локальной переменной тестового метода
+    String planningDate = generateDate(3);
+
+//    Данный метод принимает количество дней сдвига относительно текущей даты,
+//    а возвращает отформатированную строку с датой, только на валидные значения.
+//    LocalDate localDate = LocalDate.now().plusDays(3);
+//    DateTimeFormatter data = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+//    String generateDate = localDate.format(data);
 
     @Test
-    public void Test() {
+    public void test() {
         Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
         $("[placeholder='Город']").setValue("Москва");
         $("[placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-        $("[placeholder='Дата встречи']").setValue(strData);
+        $("[placeholder='Дата встречи']").setValue(planningDate);
         $("[name='name']").setValue("Иванов Иван");
         $("[name='phone']").setValue("+71231231233");
         $("[data-test-id=agreement] .checkbox__box").click();
         $(withText("Забронировать")).click();
-        $("[class='notification__content']").shouldHave(Condition.text("Встреча успешно забронирована на " + (strData)), Duration.ofSeconds(15));
+        $("[class='notification__content']").shouldHave(Condition.text("Встреча успешно забронирована на " + (planningDate)), Duration.ofSeconds(15));
     }
 }
